f98e60a7b0ee70617503fc08bcc59e122ae3e4c4
DESKTOP-SD25RDT\student <22cu0203@jec.ac.jp>
2025-06-18 19:11:16 +0900

    小見川　あげなおし

--- Assets/#Scripts/CarScript/Steering.cs
+++ Assets/#Scripts/CarScript/Steering.cs
@@ -1,27 +1,29 @@
 using UnityEngine;
+using UnityEngine.UIElements;
+using VehiclePhysics;
 
 [System.Serializable]
 public class Steering
 {
     enum SteeringType
-    { 
+    {
         Ackerman,
-		Parallel
-	}
+        Parallel
+    }
 
     [SerializeField]
     SteeringType m_type = SteeringType.Ackerman;
 
     [SerializeField]
-    float m_wheelBase = 2.59f;      //ホイールベース
+    float m_wheelBase;
     [SerializeField]
-    float m_treadWidth;             //トレッド幅
+    float m_treadWidth;
     [SerializeField]
-    float m_steeringGearRatio;      //ステアとホイールのギア比
-    [SerializeField,Range(0f,450f)]
-    float m_maxSteerAngle;          //ステアのMax角度
-    [SerializeField, Range(180f, 450f)]
-    float m_steeringRange = 450f;   //ステアの倍率
+    float m_steeringGearRatio;
+    [SerializeField, Range(0f, 900f)]
+    float m_maxSteerAngle;
+    [SerializeField, Range(180f, 900f)]
+    float m_steeringRange = 900f;
 
     /// <summary>
     /// ホイールのステア角を計算する
@@ -30,56 +32,49 @@ public class Steering
     /// <param name="_isRight">右側のホイールかどうか</param>
     public float CalcSteerAngle(in float _steerInput, bool _isRight)
     {
-        //ステアリングの角度計算
-		float steerAngle = _steerInput * m_steeringRange / m_steeringGearRatio;
+        float steerAngle = _steerInput * m_steeringRange / m_steeringGearRatio;
+        if (m_maxSteerAngle <= Mathf.Abs(steerAngle))
+        {
+            steerAngle = m_maxSteerAngle * Mathf.Sign(steerAngle);
+        }
 
-        //上限の設定
-        //if(m_maxSteerAngle <= Mathf.Abs(steerAngle))
-        //{
-        //    steerAngle = m_maxSteerAngle * Mathf.Sign(steerAngle);
-        //}
-
-        //
-        switch (m_type) 
+        switch (m_type)
         {
             case SteeringType.Ackerman:
-                return CalcAckermanAngle(steerAngle, _isRight); 
+                return CalcAckermanAngle(steerAngle, _isRight);
 
-			case SteeringType.Parallel:
-				return steerAngle;
+            case SteeringType.Parallel:
+                return steerAngle;
 
             default:
                 return 0f;
-		}
+        }
     }
 
-    //ホイールの
-    float CalcAckermanAngle(float _steerAngle,bool _isRight)
+    float CalcAckermanAngle(float _steerAngle, bool _isRight)
     {
         _steerAngle *= Mathf.Deg2Rad;
-		float angleR = Mathf.Atan(m_wheelBase * Mathf.Tan(_steerAngle) / (m_wheelBase + 0.5f * m_treadWidth * Mathf.Tan(_steerAngle))) * Mathf.Rad2Deg;
-		float angleL = Mathf.Atan(m_wheelBase * Mathf.Tan(_steerAngle) / (m_wheelBase - 0.5f * m_treadWidth * Mathf.Tan(_steerAngle))) * Mathf.Rad2Deg;
+        float angleR = Mathf.Atan(m_wheelBase * Mathf.Tan(_steerAngle) / (m_wheelBase + 0.5f * m_treadWidth * Mathf.Tan(_steerAngle))) * Mathf.Rad2Deg;
+        float angleL = Mathf.Atan(m_wheelBase * Mathf.Tan(_steerAngle) / (m_wheelBase - 0.5f * m_treadWidth * Mathf.Tan(_steerAngle))) * Mathf.Rad2Deg;
 
-        if(_steerAngle > 0f)
+        // 05/18 追加:土田
+        // 速度が遅いほどsteerFactorが大きくなるように補正
+        //float maxSpeed = 250f; // 最大速度
+        //float steerFactor = 1f;
+        //steerFactor = Mathf.Clamp01(1f - (m_vehicleController.m_KPH * m_vehicleController.m_KPH) / (maxSpeed * maxSpeed));
+
+        if (_steerAngle > 0f)
         {
             angleL = CalcAckermanOutsideAngle(angleR, _steerAngle);
         }
         else
         {
-			angleR = CalcAckermanOutsideAngle(angleL, _steerAngle);
-		}
-
-
-
-        if (_isRight)
-        {
-            return angleR;
+            angleR = CalcAckermanOutsideAngle(angleL, _steerAngle);
         }
-        else
-        {
-            return angleL;
-        }
-	}
+
+        return _isRight ? angleR : angleL;
+    }
+
 
     /// <summary>
     /// 内側の切れ角から外側の切れ角を計算する
